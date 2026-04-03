fd049cc65bcbc68ec0769e6e7b417d564252b4e7
Zik30 <ainazik.ishenbek@gmail.com>
2026-04-04 00:27:31 +0500

    chat ai 2

--- src/pages/chat/ChatPage.tsx
+++ src/pages/chat/ChatPage.tsx
@@ -1,11 +1,7 @@
-import React, { type FC, useState } from 'react';
+import React, { type FC } from 'react';
 import styles from './ChatPage.module.scss';
-import { Button } from 'shared/ui/button/Button';
-import { CustomInput } from 'shared/ui/input/Input';
-import { Typography } from 'shared/ui/typography/Typography';
-import { MessageCircle } from 'lucide-react';
-import { ChatSidebar } from 'widgets/chatSidebar/ChatSidebar';
 import { ChatMain } from 'widgets/chatMain/ChatMain';
+import { ChatSidebar } from 'widgets/chatSidebar/ChatSidebar';
 
 export const ChatPage: FC = () => {
   return (
