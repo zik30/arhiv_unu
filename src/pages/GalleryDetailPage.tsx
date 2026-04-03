1b0092a6f435938644c5e7044e5a55312ea3f830
Zik30 <ainazik.ishenbek@gmail.com>
2026-04-03 23:31:42 +0500

    ornaments

--- src/pages/GalleryDetailPage.tsx
+++ src/pages/GalleryDetailPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Ornament } from 'shared/ui/ornament/Ornament';
 import { PersonDetail } from 'widgets/personDetail/PersonDetail';
 
 const GalleryDetailPage = () => {
   return (
-    <div>
-      <PersonDetail />
+    <div style={{ position: 'relative', overflow: 'clip' }}>
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <PersonDetail />
+      </div>
+      <Ornament left={-278} top={0} />
     </div>
   );
 };
--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -1,11 +1,17 @@
+import { Ornament } from 'shared/ui/ornament/Ornament';
 import { GalleryWidget } from 'widgets/galleryWidget/GalleryWidget';
 import { SearchWidget } from 'widgets/searchWidget/SearchWidget';
 
 const GalleryPage = () => {
   return (
-    <div>
-      <SearchWidget />
-      <GalleryWidget />
+    <div style={{ position: 'relative', overflow: 'clip' }}>
+      <div style={{ position: 'relative', zIndex: 1 }}>
+        <SearchWidget />
+        <GalleryWidget />
+      </div>
+      <Ornament top={-0} center />
+      <Ornament right={-398} top={1490} />
+      <Ornament left={-378} top={3460} />
     </div>
   );
 };
